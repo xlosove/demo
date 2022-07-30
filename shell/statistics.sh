892d79d45968a8243afa9d87f679ca1a4294a3b8
劲楷小书 <xlosove@outlook.com>
2022-07-30 23:33:16 +0800

    2

--- shell/statistics.sh
+++ shell/statistics.sh
@@ -2,18 +2,19 @@
 
 # 项目目录
 ITEM_PATH=D:/MyProject/demo
-# 统计结果
-curRecordFile=${ITEM_PATH}/out/gitCodeStat-$(date "+%Y%m")".csv"
+# 结果文件
+recordFile=${ITEM_PATH}/codeStat-$(date "+%Y%m")".csv"
+# 统计时间
 CURRENT_TIME=$(date "+%Y-%m-%d %H:%M:%S")
 # 统计周期(天)
 STAT_DURATION_DAY=1
 
 # 统计代码
-function statCode() {
-    echo "current record file name: $curRecordFile"
+function statCode() {       
+    echo "current record file name: $recordFile"
 
-    rm -f $curRecordFile
-    echo "统计时间, 统计周期, 提交人, 提交次数, 增加行数, 删除行数, 行数差" >>$curRecordFile
+    rm -f $recordFile
+    echo "统计时间, 统计周期, 提交人, 提交次数, 增加行数, 删除行数, 行数差" >>$recordFile
 
     cd $ITEM_PATH
     git fetch --all
@@ -25,7 +26,7 @@ function statCode() {
         echo -en "$CURRENT_TIME, $STAT_DURATION_DAY, $name"
         git log --all --since=${STAT_DURATION_DAY}.day.ago --author="$name" --pretty=oneline | awk -vsum=0 '{ sum += 1 } END { printf ", %s,", sum }' -
         git log --all --since=${STAT_DURATION_DAY}.day.ago --author="$name" --pretty=tformat: --numstat | awk -vadd=0 -vsubs=0 -vloc=0 '{ add += $1; subs += $2; loc += $1 - $2 } END { printf " %s, %s, %s \n", add, subs, loc }' -
-    done >>$curRecordFile
+    done >>$recordFile
 }
 
 # 启动脚本
